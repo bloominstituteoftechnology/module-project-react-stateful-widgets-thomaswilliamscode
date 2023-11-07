f2cd978721be6eda835dbe2d1bdcb1c353a5edb7
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2023-11-07 10:21:48 -0500

    counter steps 0,1,& 2 complete

--- frontend/components/Counter.js
+++ frontend/components/Counter.js
@@ -45,10 +45,11 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Counter() {
   /* STEP 1 */
+  const [count, setCount] = useState(0);
 
   const increment = () => {
     /* STEP 4 */
@@ -60,10 +61,17 @@ export default function Counter() {
     /* STEP 6 */
   };
 
+  const isEven = (num) => {
+    return num % 2 === 0
+  }
+
+  const color = isEven(count) ? 'royalblue' : 'crimson'
   const style = {
+    
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: color, 
+    
   };
 
   return (
